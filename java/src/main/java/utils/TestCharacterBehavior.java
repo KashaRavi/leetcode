18dbbb7bd1b9f6104c7769d3e93aba084547c1c5
Ravi Kasha <ravi.kasha1990@gmail.com>
2017-02-19 13:21:53 +0530

    save all local files. Nothing much intellectual.

---
+++ java/src/main/java/utils/TestCharacterBehavior.java
@@ -0,0 +1,22 @@
+package utils;
+
+/**
+ * Created by Ravi Kasha on 10-09-2016.
+ */
+public class TestCharacterBehavior {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello!uioipopopopuiuiuio");
+        System.out.print("Hello!");
+        System.out.print("\b");
+        System.out.print("\b");
+        System.out.print("\b");
+        System.out.print("\b");
+
+        Thread.sleep(1000);
+//        System.out.print((char)13);
+//        System.out.print("\r");
+        Thread.sleep(1000);
+        System.out.print("....");
+        System.out.print("Hello");
+    }
+}
